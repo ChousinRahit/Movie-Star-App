68c0670b845639b74f7ef68025f2eed8ebe4480f
ChousinRahit <2428kcr@gmail.com>
2020-05-07 21:43:51 +0530

    Updates

---
+++ precache-manifest.748432a85b9540688a42d9126cd79045.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837a4caaf3b5a64b57af5671caadd6e3",
+    "url": "/Movie-Star-App/index.html"
+  },
+  {
+    "revision": "e81cfb8f223b984d7c0c",
+    "url": "/Movie-Star-App/static/css/2.27fdde94.chunk.css"
+  },
+  {
+    "revision": "2316ecb3edb1eb2c5939",
+    "url": "/Movie-Star-App/static/css/main.cbbd6b1f.chunk.css"
+  },
+  {
+    "revision": "e81cfb8f223b984d7c0c",
+    "url": "/Movie-Star-App/static/js/2.a1f47b25.chunk.js"
+  },
+  {
+    "revision": "38b6d0d0f8626b4800a346e0061c2296",
+    "url": "/Movie-Star-App/static/js/2.a1f47b25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47d21d45966dd9e262f8",
+    "url": "/Movie-Star-App/static/js/3.5ab9ff83.chunk.js"
+  },
+  {
+    "revision": "2316ecb3edb1eb2c5939",
+    "url": "/Movie-Star-App/static/js/main.ac534705.chunk.js"
+  },
+  {
+    "revision": "f22f56f4ef6bfa5b3ff6",
+    "url": "/Movie-Star-App/static/js/runtime-main.d7479836.js"
+  },
+  {
+    "revision": "ab6345ba36bd01d949bcbbfbc5c8b1ab",
+    "url": "/Movie-Star-App/static/media/no_pic_F.ab6345ba.jpg"
+  },
+  {
+    "revision": "dbfd82aeb8c0c4479a2dc4b6de4434c0",
+    "url": "/Movie-Star-App/static/media/no_pic_M.dbfd82ae.jpeg"
+  },
+  {
+    "revision": "02a9430b88975cae16fcfcc9cf7b5799",
+    "url": "/Movie-Star-App/static/media/themovieorg.02a9430b.svg"
+  }
+]);
